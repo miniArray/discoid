6b6972e64176fafc67b2ce47a1db04d42cd60eea
Simon W. Jackson <simon@simonwjackson.com>
2016-01-26 20:08:42 +0100

    refactor(buildVolObject): Decouple the volume object contructor
    Refactored the code that builds an object that contains info for a given volume.

--- test/mountvol.js
+++ test/mountvol.js
@@ -22,15 +22,18 @@ describe('mountvol', () => {
         it('should have a specific objects', () => {
             volumes.should.eventually.include({
                 'guid': '6f35a39a-4c5d-4208-ba6e-aa8d2a2add96',
+                'unc': '\\\\?\\Volume{6f35a39a-4c5d-4208-ba6e-aa8d2a2add96}\\',
                 'mounts': ['C:\\']
             });
 
             volumes.should.eventually.include({
-                'guid': 'd9082c7e-c103-11e5-82cd-605718ebe2c1'
+                'guid': 'd9082c7e-c103-11e5-82cd-605718ebe2c1',
+                'unc': '\\\\?\\Volume{d9082c7e-c103-11e5-82cd-605718ebe2c1}\\'
             });
 
             volumes.should.eventually.include({
-                'guid': 'b0357aa2-c106-11e5-82ce-605718ebe2c1'
+                'guid': 'b0357aa2-c106-11e5-82ce-605718ebe2c1',
+                'unc': '\\\\?\\Volume{b0357aa2-c106-11e5-82ce-605718ebe2c1}\\'
             });
         });
     });
